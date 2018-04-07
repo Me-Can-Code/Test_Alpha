c6f9f363de095696ea675810da71cd0dc944638f
Me-Can-Code <nithin27jan@gmail.com>
2018-04-06 21:41:45 -0400

    Hi. This is a prototype code.

---
+++ Hello.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+string a;
+cin >> a ;
+cout << a ;
+    return 0;
+}
